a4eb68cc8ea6ac3e405deaf9fd6229ca44f30c39
Charon <charon.s.tom@gmail.com>
2025-01-29 11:51:58 +0100

    f4

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { JwtGuard } from './auth/guards/jwt.guard';
-
+//testpush
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalGuards(new JwtGuard(app.get(Reflector)));
